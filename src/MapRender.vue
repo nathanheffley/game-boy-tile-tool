5a4f57adcc1cceb6caf48aa2f6f83417d283c135
Nathan Heffley <nathan@nathanheffley.com>
2023-11-29 00:16:01 -0500

    Get panning and zooming working

--- src/MapRender.vue
+++ src/MapRender.vue
@@ -18,16 +18,36 @@ const colors = {
 }
 
 const mouseDown = ref(false)
+const middleDown = ref(false)
+
+const xScale = ref(3)
+const yScale = ref(3)
+
+const xOffset = ref(8)
+const yOffset = ref(8)
 
 const sendPaintEvent = (event) => {
     const rect = canvas.value.getBoundingClientRect()
-    const x = Math.floor((event.clientX - rect.left) / 3 / 8)
+    const x = Math.floor((event.clientX - rect.left - xOffset.value) / xScale.value / 8)
     if (x < 0 || x >= props.mapWidth) return
-    const y = Math.floor((event.clientY - rect.top) / 3 / 8)
+    const y = Math.floor((event.clientY - rect.top - yOffset.value) / yScale.value / 8)
     if (y < 0 || y >= props.mapHeight) return
     const index = x + y * props.mapWidth
     if (index < 0 || index >= props.map.length) return
     emit('paint', index)
+}
+
+const oldMouseX = ref(null)
+const oldMouseY = ref(null)
+
+const dragView = (event) => {
+    if (oldMouseX.value !== null && oldMouseY.value !== null) {
+        xOffset.value += (event.clientX - oldMouseX.value)
+        yOffset.value += (event.clientY - oldMouseY.value)
+        render()
+    }
+    oldMouseX.value = event.clientX
+    oldMouseY.value = event.clientY
 }
 
 const canvas = ref(null)
@@ -36,11 +56,11 @@ const render = () => {
     if (!canvas.value) return
 
     const ctx = canvas.value.getContext('2d')
+    ctx.resetTransform()
+    ctx.clearRect(0, 0, canvas.value.width, canvas.value.height);
+    ctx.transform(xScale.value, 0, 0, yScale.value, xOffset.value, yOffset.value)
+
     const computedStyle = getComputedStyle(canvas.value)
-
-    ctx.fillStyle = '#fff'
-    ctx.fillRect(0, 0, canvas.value.width, canvas.value.height)
-
     for (let x = 0; x < props.mapWidth; x++) {
         for (let y = 0; y < props.mapHeight; y++) {
             const sprite = props.sprites[props.map[x + y * props.mapWidth]] ?? 0
@@ -60,18 +80,31 @@ watch(() => props.mapHeight, render)
 watch(() => props.sprites, render, { deep: true })
 
 onMounted(() => {
-    const ctx = canvas.value.getContext('2d')
-    ctx.scale(3, 3)
     render()
 })
 </script>
 
 <template>
     <canvas
-        @mousedown="(event) => {mouseDown = true; sendPaintEvent(event)}"
-        @mousemove="(event) => {if (mouseDown) sendPaintEvent(event)}"
-        @mouseleave="mouseDown = false"
-        @mouseup="mouseDown = false"
+        @mousedown.left="(event) => {mouseDown = true; sendPaintEvent(event)}"
+        @mouseup.left="mouseDown = false"
+        @mousedown.middle="middleDown = true"
+        @mouseup.middle="middleDown = false; oldMouseX = null; oldMouseY = null"
+        @wheel="event => {
+            if (event.deltaY > 0) {
+                xScale = Math.max(1, xScale - 1)
+                yScale = Math.max(1, yScale - 1)
+            } else {
+                xScale = Math.min(5, xScale + 1)
+                yScale = Math.min(5, yScale + 1)
+            }
+            render()
+        }"
+        @mousemove="(event) => {
+            if (mouseDown) { sendPaintEvent(event) }
+            else if (middleDown) { dragView(event) }
+        }"
+        @mouseleave="mouseDown = false; middleDown = false"
         class="border-2 border-gray-200 select-none w-[333px] h-[333px]"
         width="333"
         height="333"
